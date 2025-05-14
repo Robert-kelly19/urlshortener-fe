18a9364e5b2643b39ef49b01408a79bfccbd2776
akuma19 <kellyhandy31@gmail.com>
2025-05-14 23:09:54 +0100

    impleted user registration and login

---
+++ pages/login.jsx
@@ -0,0 +1,101 @@
+import { useFormik } from "formik";
+import { useNavigate } from "react-router";
+import * as Yup from "yup";
+import axios from "axios";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const validateSchema = Yup.object({
+    email: Yup.string().email("invalid email").required("email is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .matches(/[a-z]/, "At least one lowercase letter required")
+      .matches(/[A-Z]/, "At least one uppercase letter required")
+      .matches(/[0-9]/, "At least one number required")
+      .matches(/[@$!%*?&#]/, "At least one special character required"),
+  });
+
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    try {
+      const res = await axios.post(
+        `http://localhost:8000/auth/login`,
+        values
+      );
+      console.log("form submitted", res.data);
+      resetForm();
+    } catch (error) {
+      console.error("error while submitting:", error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validateSchema,
+    onSubmit: handleSubmit,
+  });
+  return (
+    <>
+      <div className="container">
+        <div className="item">
+          <div className="item1">
+            <div className="item1-1">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/2615/2615096.png"
+                alt="logo"
+              />
+              <h1>
+                {" "}
+                <span>Url</span>Shortener
+              </h1>
+            </div>
+            <p>Get a short Url for your links</p>
+          </div>
+          <div className="item2">
+            <form onSubmit={formik.handleSubmit}>
+              <div>
+                <label>Email</label>
+                <br />
+                <input
+                  type="email"
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                {formik.touched.email && formik.errors.email && (
+                  <div style={{ color: "red" }}>{formik.errors.email}</div>
+                )}
+              </div>
+
+              <div>
+                <label>Password</label>
+                <br />
+                <input
+                  type="password"
+                  name="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+                {formik.touched.password && formik.errors.password && (
+                  <div style={{ color: "red" }}>{formik.errors.password}</div>
+                )}
+              </div>
+
+              <button type="submit" disabled={formik.isSubmitting}>
+                {formik.isSubmitting ? "loggingin..." : "login"}
+              </button>
+            </form>
+          </div>
+          <hr />
+          <button onClick={()=>navigate('/signup')}>Create New Account</button>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ pages/registration.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router";
+import axios from "axios";
+
+export default function Signup() {
+    const navigate = useNavigate();
+  const validateSchema = Yup.object({
+    firstName: Yup.string().min(3).required("firstName is required"),
+    lastName: Yup.string().min(3).required("lastName is required"),
+    email: Yup.string().email().required("email is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .matches(/[a-z]/, "At least one lowercase letter required")
+      .matches(/[A-Z]/, "At least one uppercase letter required")
+      .matches(/[0-9]/, "At least one number required")
+      .matches(/[@$!%*?&#]/, "At least one special character required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Please confirm your password"),
+  });
+
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    try {
+      const res = await fetch(`http://localhost:8000/auth/register`, ({
+        method:"post",
+        headers:{'content-Type': 'application/json',},
+        body:JSON.stringify(values)
+      }));
+      const data = await res.json();
+      console.log("submited:", data);
+      resetForm();
+    } catch (error) {
+      console.error("error while registatring:", error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmpassword: "",
+    },
+    validateSchema,
+    onSubmit: handleSubmit,
+  });
+  return (
+    <>
+      <div className="contain">
+        <div className="con1">
+          <div className="item1-1">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2615/2615096.png"
+              alt="logo"
+            />
+            <h1>
+              <span>Url</span>Shortener
+            </h1>
+          </div>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="names">
+              <div>
+                <label htmlFor="firstName">FirstName</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="enter your firstname"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.firstName}
+                />
+                {formik.touched.firstName && formik.errors.firstName && (
+                  <div style={{ color: "red" }}>{formik.errors.firstName}</div>
+                )}
+              </div>
+              <div>
+                <label htmlFor="lastName">lastName</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="enter your last name"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.lastName}
+                />
+                {formik.touched.lastName && formik.errors.lastName && (
+                  <div style={{ color: "red" }}>{formik.errors.lastName}</div>
+                )}
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="enter your email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <div style={{ color: "red" }}>{formik.errors.email}</div>
+              )}
+            </div>
+            <div className="password">
+              <div>
+                {" "}
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="enter your password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+                {formik.touched.password && formik.errors.password && (
+                  <div style={{ color: "red" }}>{formik.errors.password}</div>
+                )}
+              </div>
+              <div>
+                {" "}
+                <label htmlFor="confirmpassword">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmpassword"
+                  placeholder="confirm your password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.confirmpassword}
+                />
+                {formik.touched.confirmpassword &&
+                  formik.errors.confirmpassword && (
+                    <div style={{ color: "red" }}>
+                      {formik.errors.confirmpassword}
+                    </div>
+                  )}
+              </div>
+            </div>
+            <button type="submit" id="signup" disabled={formik.isSubmitting}>
+              {formik.isSubmitting ? "Signing Up..." : "Sign Up"}
+            </button>
+            <p onClick={()=>navigate('/')}>Already have an account</p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
