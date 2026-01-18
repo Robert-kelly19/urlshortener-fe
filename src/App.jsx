262c4bd832940fdabb7aa012cd4956a2cfe53fc2
akuma19 <kellyhandy31@gmail.com>
2026-01-18 19:18:05 +0100

    feat: added deployed back end url1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Signup from '../pages/registration.jsx'
 import Home from  '../pages/home.jsx'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router'
-import Landing from '../pages/Landing.jsx'
+import Landing from '../pages/landing.jsx'
 
 
 function App() {
